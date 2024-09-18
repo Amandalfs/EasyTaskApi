a75c84d9e130217695907d434bd1feb6592b6c49
Amandalfs <amandalfs57@gmail.com>
2024-09-18 19:49:19 -0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './env';
+
+@Module({
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
